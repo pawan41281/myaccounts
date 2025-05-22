5093fcf4790d0b1d7f89017798e97a29bbf823d9
Pawan Kumar <pawan41281@gmail.com>
2025-05-22 10:01:53 +0530

    initial commit

---
+++ src/main/java/org/myaccounts/MyaccountsApplication.java
@@ -0,0 +1,13 @@
+package org.myaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyaccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyaccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/myaccounts/MyaccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyaccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
